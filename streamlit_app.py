8b300d54363c2a7df913ad1f264012a92cba46aa
collin <collinvaccaro00@gmail.com>
2025-07-22 20:52:19 +0000

    fixed session

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,9 +13,8 @@ st.write('The name on your Smoothie will be: ', name_on_order)
  
 cnx = st.connection("snowflake")
 session = cnx.session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 
-session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
  
 ingredients_list = st.multiselect(
